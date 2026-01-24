f4171aa0aefffad308ab108a5d52ba4f58ee1b87
y-tetsu <45305605+y-tetsu@users.noreply.github.com>
2026-01-24 23:48:39 +0900

    add sprite animation

--- soulrun.py
+++ soulrun.py
@@ -4,7 +4,33 @@ pygame.init()
 screen = pygame.display.set_mode((400, 300))
 clock = pygame.time.Clock()
 
+last_switch = pygame.time.get_ticks()
+current_frame = 0
+frames = [
+    pygame.image.load("frame0.png").convert_alpha(),
+    pygame.image.load("frame1.png").convert_alpha(),
+]
+
 running = True
 while running:
-    # 画面を更新する処理
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # 画面をクリア
+    screen.fill((0, 0, 0))
+
+    # キャラを表示
+    now = pygame.time.get_ticks()
+    if now - last_switch >= 200:
+        current_frame = (current_frame + 1) % len(frames)
+        last_switch = now
+    screen.blit(frames[current_frame], (135, 95))
+
+    # 画面を更新
+    pygame.display.flip()
+
+    # 待ち時間
     clock.tick(60)
+
+pygame.quit()
